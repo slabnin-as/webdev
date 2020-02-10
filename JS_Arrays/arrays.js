7b52d50d1be7570dc3718e1e723286ad773403f9
Anton Slabnin <insk54@gmail.com>
2020-02-11 01:28:50 +0700

    Работа с массивами bug fix;

--- JS_Arrays/arrays.js
+++ JS_Arrays/arrays.js
@@ -1,57 +1,51 @@
-var numbersArray = [4, -3, 1, 98, 45, 87, 34];
+(function () {
+    let numbersArray = [4, -3, 1, 98, 45, 87, 34];
 
-descendingArraySort(numbersArray);
-console.log(numbersArray);
-console.log(getFirstFiveElements(numbersArray));
-console.log(getLastFiveElements(numbersArray));
-console.log(sumEvenNumber(numbersArray));
+    descendingArraySort(numbersArray);
+    console.log(numbersArray);
+    console.log(getFirstFiveElements(numbersArray));
+    console.log(getLastFiveElements(numbersArray));
+    console.log(sumEvenNumber(numbersArray));
 
-var array2 = [];
-fillArray(1, 100, array2);
+    let array2 = [];
+    fillArray(1, 100, array2);
 
-console.log(array2);
-console.log(getSquaredEvenNumber(array2));
+    console.log(array2);
+    console.log(getSquaredEvenNumberList(array2));
 
-function descendingArraySort(array) {
-    array.sort(function(a, b) {
-        return b - a;
-    });
-}
-
-function getFirstFiveElements(array) {
-    return array.slice(0, 5);
-}
-
-function getLastFiveElements(array) {
-    return array.slice(-5);
-}
-
-function sumEvenNumber(array) {
-    var sum = 0;
-
-    array.forEach(function(item) {
-        if (item % 2 === 0) {
-            sum += item;
-        }
-    });
-
-    return sum;
-}
-
-function fillArray(start, end, array) {
-    for (var i = start; i <= end; i++) {
-        array.push(i);
+    function descendingArraySort(array) {
+        array.sort(function (a, b) {
+            return b - a;
+        });
     }
-}
 
-function getSquaredEvenNumber(array) {
-    var result = [];
+    function getFirstFiveElements(array) {
+        return array.slice(0, 5);
+    }
 
-    array.forEach(function(item) {
-        if (item % 2 === 0) {
-            result.push(item * item);
+    function getLastFiveElements(array) {
+        return array.slice(-5);
+    }
+
+    function sumEvenNumber(array) {
+        return array.reduce(function (sum, item) {
+            return sum + (item % 2 === 0 ? item : 0);
+        });
+    }
+
+    function fillArray(start, end, array) {
+        for (let i = start; i <= end; i++) {
+            array.push(i);
         }
-    });
+    }
 
-    return result;
-}
+    function getSquaredEvenNumberList(array) {
+        let evenNumbers = array.filter(function (number) {
+            return number % 2 === 0;
+        });
+
+        return evenNumbers.map(function (number) {
+            return number * number;
+        });
+    }
+})();
