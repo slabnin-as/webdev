c3426a62c8c6e51c4121221b0469a22cf8634449
Anton Slabnin <insk54@gmail.com>
2020-03-16 01:25:52 +0700

    TODO List version 0.9.6

--- JS_Objects/objects.js
+++ JS_Objects/objects.js
@@ -1,88 +1,96 @@
 "use strict";
 
 (function () {
-var countries = [
-{
-    name: "Russia",
-    cities: [
+    var countries = [
         {
-            name: "Moscow",
-            population: 12615882
+            name: "Russia",
+            cities: [
+                {
+                    name: "Moscow",
+                    population: 12615882
+                },
+                {
+                    name: "Novosibirsk",
+                    population: 1618039
+                },
+                {
+                    name: "Omsk",
+                    population: 1164815
+                },
+                {
+                    name: "Tomsk",
+                    population: 575352
+                }
+            ]
         },
         {
-            name: "Novosibirsk",
-            population: 1618039
+            name: "Spain",
+            cities: [
+                {
+                    name: "Madrid",
+                    population: 3265038
+                },
+                {
+                    name: "Barcelona",
+                    population: 1615448
+                },
+                {
+                    name: "Malaga",
+                    population: 568030
+                }
+            ]
         },
         {
-            name: "Omsk",
-            population: 1164815
-        },
-        {
-            name: "Tomsk",
-            population: 575352
+            name: "USA",
+            cities: [
+                {
+                    name: "New-York",
+                    population: 8398748
+                },
+                {
+                    name: "Miami",
+                    population: 470914
+                },
+                {
+                    name: "San Francisco",
+                    population: 883305
+                },
+                {
+                    name: "Dallas",
+                    population: 1345047
+                }
+            ]
         }
-    ]
-},
-{
-    name: "Spain",
-    cities: [
-        {
-            name: "Madrid",
-            population: 3265038
-        },
-        {
-            name: "Barcelona",
-            population: 1615448
-        },
-        {
-            name: "Malaga",
-            population: 568030
-        }
-    ]
-},
-{
-    name: "USA",
-    cities: [
-        {
-            name: "New-York",
-            population: 8398748
-        },
-        {
-            name: "Miami",
-            population: 470914
-        },
-        {
-            name: "San Francisco",
-            population: 883305
-        },
-        {
-            name: "Dallas",
-            population: 1345047
-        }
-    ]
-}
-];
+    ];
 
-function getMaxCitiesCountries(countriesList) {
-    const citiesMaximum = Math.max.apply(null, countriesList.map(function (country){return country.cities.length}));
-    var countriesNames = countriesList
-                        .filter(function (country){return country.cities.length === citiesMaximum;})
-                        .map(function (country) {return country.name;});
+    function getMaxCitiesCountries(countriesList) {
+        const citiesMaximum = Math.max.apply(null, countriesList.map(function (country) {
+            return country.cities.length
+        }));
+        var countriesNames = countriesList
+            .filter(function (country) {
+                return country.cities.length === citiesMaximum;
+            })
+            .map(function (country) {
+                return country.name;
+            });
 
-    return countriesNames;
-}
+        return countriesNames;
+    }
 
-function getCountryPopulation(countriesList) {
-    var countriesPopulation = {};
+    function getCountryPopulation(countriesList) {
+        var countriesPopulation = {};
 
-    countriesList.forEach(function(country) {
-        var name = country.name;
-        countriesPopulation[name] = country.cities.reduce(function (sum, city) {return sum + city.population;}, 0);
-    });
+        countriesList.forEach(function (country) {
+            var name = country.name;
+            countriesPopulation[name] = country.cities.reduce(function (sum, city) {
+                return sum + city.population;
+            }, 0);
+        });
 
-    return countriesPopulation;
-}
+        return countriesPopulation;
+    }
 
-console.table(getMaxCitiesCountries(countries));
-console.table(getCountryPopulation(countries));
+    console.table(getMaxCitiesCountries(countries));
+    console.table(getCountryPopulation(countries));
 })();
--- TODOList/todoList.js
+++ TODOList/todoList.js
@@ -2,32 +2,53 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     var noteList = document.getElementById("todo-list");
-
+    var inputText = document.getElementById("input-field");
     var button = document.getElementById("add-note");
-    button.addEventListener("click", function (e){
-        var inputText = document.getElementById("input-field").value;
 
-        if(inputText === ""){
+    button.addEventListener("click", function (e) {
+        var newText = inputText.value;
+
+        if (newText === "") {
             return;
         }
 
         var note = document.createElement("li");
-        note.innerHTML = inputText + "<button type='button' style='margin-left: 40px'>X</button>" +
-                                        "<button type='button'>Редактировать</button>";
+        note.style.paddingBottom = "5px";
+        addNote(newText);
+
+        function addNote(text) {
+            note.innerHTML = "<span></span><button type='button' style='margin-left: 40px;'>Удалить</button>" +
+                "<button type='button' style='margin-left: 5px;'>Редактировать</button>";
+
+            var textLine = note.children[0];
+            textLine.textContent = text;
+
+            var deleteButton = note.children[1];
+            deleteButton.addEventListener("click", function (e) {
+                noteList.removeChild(note);
+            });
+
+            var editButton = note.children[2];
+            editButton.addEventListener("click", function (e) {
+                note.innerHTML = "<input type='text'><button type='button' style='margin-left: 5px;'>Отменить</button>" +
+                    "<button type='button' style='margin-left: 5px;'>Сохранить</button>";
+
+                var editTextLine = note.children[0];
+
+                var cancelButton = note.children[1];
+                cancelButton.addEventListener("click", function (e) {
+                    addNote(text);
+                });
+
+                var saveButton = note.children[2];
+                saveButton.addEventListener("click", function (e) {
+                    var editingText = editTextLine.value;
+                    addNote(editingText);
+                });
+
+            });
+        }
+
         noteList.appendChild(note);
-
-        var deleteButton = note.children[0];
-        deleteButton.addEventListener("click", function(e){
-            noteList.removeChild(note);
-        });
-
-        var editButton = note.children[1];
-        editButton.addEventListener("click", function(e){
-            note.innerHTML = "<input type='text'><button type='button' class='buttons'>Отменить</button>" +
-                                                 "<button type='button' class='buttons'>Сохранить</button>";
-
-        });
-
     });
-
 });
