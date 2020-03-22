ade82dd028687d853e89f8826ccf63a46c0c527b
Anton Slabnin <insk54@gmail.com>
2020-03-22 16:38:04 +0700

    TODO List исправление ошибок.

--- TODOList/todoList.js
+++ TODOList/todoList.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var inputText = document.getElementById("input-field");
     var button = document.getElementById("add-note");
 
-    button.addEventListener("click", function (e) {
+    button.addEventListener("click", function () {
         var newText = inputText.value;
 
         if (newText === "") {
@@ -14,40 +14,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var note = document.createElement("li");
         note.style.paddingBottom = "5px";
-        addNote(newText);
 
         function addNote(text) {
-            note.innerHTML = "<span></span><button type='button' style='margin-left: 40px;'>Удалить</button>" +
-                "<button type='button' style='margin-left: 5px;'>Редактировать</button>";
+            note.innerHTML = "<span></span><button type='button' class='delete-button'>Удалить</button>" +
+                "<button type='button' class='button'>Редактировать</button>";
 
             var textLine = note.children[0];
             textLine.textContent = text;
 
             var deleteButton = note.children[1];
-            deleteButton.addEventListener("click", function (e) {
+            deleteButton.addEventListener("click", function () {
                 noteList.removeChild(note);
             });
 
             var editButton = note.children[2];
-            editButton.addEventListener("click", function (e) {
-                note.innerHTML = "<input type='text'><button type='button' style='margin-left: 5px;'>Отменить</button>" +
-                    "<button type='button' style='margin-left: 5px;'>Сохранить</button>";
+            editButton.addEventListener("click", function () {
+                note.innerHTML = "<input type='text'><button type='button' class='button'>Отменить</button>" +
+                    "<button type='button' class='button'>Сохранить</button>";
 
                 var editTextLine = note.children[0];
+                editTextLine.value = text;
 
                 var cancelButton = note.children[1];
-                cancelButton.addEventListener("click", function (e) {
+                cancelButton.addEventListener("click", function () {
                     addNote(text);
                 });
 
                 var saveButton = note.children[2];
-                saveButton.addEventListener("click", function (e) {
+                saveButton.addEventListener("click", function () {
                     var editingText = editTextLine.value;
                     addNote(editingText);
                 });
 
             });
         }
+
+        addNote(newText);
+        inputText.value = "";
+        inputText.focus();
 
         noteList.appendChild(note);
     });
