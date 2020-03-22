bd76653e80079187a55ab916210dd1f69aa9aa37
Anton Slabnin <insk54@gmail.com>
2020-03-22 16:19:23 +0700

    Конвертер температур, исправление ошибок.

--- TemperatureConverter/converter.js
+++ TemperatureConverter/converter.js
@@ -1,26 +1,26 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-var celsius = document.getElementById("input-celsius");
-var fahrenheit = document.getElementById("output-fahrenheit");
-var kelvin = document.getElementById("output-kelvin");
+    var celsius = document.getElementById("input-celsius");
+    var fahrenheit = document.getElementById("output-fahrenheit");
+    var kelvin = document.getElementById("output-kelvin");
 
-var validationMessage = document.getElementById("validation-message");
+    var validationMessage = document.getElementById("validation-message");
 
-var button = document.getElementById("convert-button");
-button.addEventListener("click", function(e){
-    var inputTemp = celsius.value;
+    var button = document.getElementById("convert-button");
+    button.addEventListener("click", function () {
+        var inputCelsiusTemperature = celsius.value;
 
-    if(isNaN(inputTemp) || inputTemp === ""){
-        celsius.value = "";
-        fahrenheit.value = "";
-        kelvin.value = "";
-        validationMessage.style.display = "block";
-        return;
-    }
+        if (isNaN(inputCelsiusTemperature) || inputCelsiusTemperature === "") {
+            celsius.value = "";
+            fahrenheit.value = "";
+            kelvin.value = "";
+            validationMessage.style.display = "block";
+            return;
+        }
 
-    validationMessage.style.display = "none";
-    fahrenheit.value = +inputTemp*9/5 + 32;
-    kelvin.value = +inputTemp+273.15;
-});
+        validationMessage.style.display = "none";
+        fahrenheit.value = +inputCelsiusTemperature * 9 / 5 + 32;
+        kelvin.value = +inputCelsiusTemperature + 273.15;
+    });
 });
