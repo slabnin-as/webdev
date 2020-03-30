e8d8a860b2cd9e571831bf2bd3902888129caed5
Anton Slabnin <insk54@gmail.com>
2020-03-30 11:37:35 +0700

    TODO List jQuery 1.0

--- TODOList_jQuery/todoList.js
+++ TODOList_jQuery/todoList.js
@@ -1,8 +1,45 @@
-"use strict";
-
 $(function () {
     $("#add-note").click(function () {
-        var inputText = $("#input-field").val();
-        console.log(inputText);
+        var userInputField = $("#input-field");
+        var inputText = userInputField.val();
+
+        if (inputText === "") {
+            return;
+        }
+
+        var note = $("<li>");
+
+        function addNote(text) {
+            note.html("<span></span><button type='button' class='delete-button'>Удалить</button>" +
+                "<button type='button' class='edit-button'>Редактировать</button>");
+
+            note.children("span").text(text);
+
+            note.children(".delete-button").click(function () {
+                note.remove();
+            });
+
+            note.children(".edit-button").click(function () {
+                note.html("<input type='text'><button type='button' class='cancel-button'>Отменить</button>" +
+                    "<button type='button' class='save-button'>Сохранить</button>");
+
+                note.children("input[type=text]").val(text);
+
+                note.children(".cancel-button").click(function () {
+                    addNote(text);
+                });
+
+                note.children(".save-button").click(function () {
+                    var newText = note.children("input[type=text]").val();
+                    addNote(newText);
+                });
+            });
+        }
+
+        addNote(inputText);
+        userInputField.val("");
+        userInputField.focus();
+
+        $("#todo-list").append(note);
     });
 });
